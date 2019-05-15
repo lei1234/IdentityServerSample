4068f451f089c3b43c0e098b5e6a19338b2e6b22
adamson <2524978214@qq.com>
2019-05-15 20:43:52 +0800

    init project

---
+++ IdentityServerCenter/MyConfig.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerCenter
+{
+    public class MyConfig
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("orderApi"),
+                //如果资源api支持reference需要单独配置secret,userApi支持jwt reference两种
+                new ApiResource("userApi"){ApiSecrets={new Secret("userapiSecret".Sha256()) } }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId="jwt",
+                    ClientSecrets={new Secret("jwt-secret".Sha256()) },
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    //配置客户端的token验证方式
+                    //eyJhbGciOiJSUzI1NiIsImtpZCI6IjViY2UyZjljNTBhMWRiZTc4MGE2YTM2NTlmZjdiNDY2IiwidHlwIjoiSldUIn0.eyJuYmYiOjE1NTc5MjI5MTcsImV4cCI6MTU1NzkyNjUxNywiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjpbImh0dHA6Ly9sb2NhbGhvc3Q6NTAwMC9yZXNvdXJjZXMiLCJvcmRlckFwaSJdLCJjbGllbnRfaWQiOiJqd3QiLCJzY29wZSI6WyJvcmRlckFwaSJdfQ.ECYvZn7pMUxLOcZLgA1LX1oGu8YWspVhmOubBVlWG_tCtP7Wcpr5gHdM1hY5ZA62rKfUP8QgflDz3vTrfhhMN7In8Y3ySaFruCPSDtXdp9t2upbB5OFVkdTT_1M7ubSTHcPDGjH5LqcxxrU_0pViXqb00w0a_vRamr4CLX6be0p7Jd7b5JOoS0WQNYPs2jiKMSO6GJXonrAPhdW5MlZmX7E_sfoYjjWi76XanGz5pyS_s02zCMaOOzJXDqVp2c1WS4Q6o3vGStcRqodRWsrXBSd0bJMAejDaVGXTKT514XonyTKzk-sBC4m5uEpAI72QVr3_O1NQdJJdUrzeJ78GRA
+                    AccessTokenType=AccessTokenType.Jwt,
+                    AllowedScopes={ "orderApi" ,"userApi"}
+               },
+                new Client
+                {
+                    ClientId="reference",
+                    ClientSecrets={new Secret("reference-secret".Sha256()) },
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    //571542530ec862317aa995b1565bc1a080984970e6fcf81f8ae7df2b8a1a338e
+                    AccessTokenType=AccessTokenType.Reference,
+                    //由于orderApi只支持jwt，而客户端是reference，所以访问不了orderApi
+                    AllowedScopes={ "orderApi" ,"userApi"}
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> GetTestUser()
+        {
+            return new List<TestUser>
+            {
+
+            };
+        }
+    }
+}
---
+++ UserApi/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace UserApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //userApi 支持jwt reference
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.ApiName = "userApi";
+                    options.ApiSecret = "userapiSecret";
+                    options.RequireHttpsMetadata = false;
+
+                    //支持两种方式
+                    //reference 验证路径 http://localhost:5000/connect/introspect
+                    //reference 撤销路径 http://localhost:5000/connect/revocation
+                    options.SupportedTokens = SupportedTokens.Both;
+
+                    //配置缓存
+                    options.EnableCaching = true;
+                    options.CacheDuration = TimeSpan.FromMinutes(10);
+                });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
